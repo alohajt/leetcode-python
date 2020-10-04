064155dc4ee56c7223be1a047f3fa0a365149658
alohajt <alohajiatao@gmail.com>
2020-10-04 14:36:22 -0600

    169

---
+++ 5. divide and conquer/169. Majority Element.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count = collections.Counter(nums)
+        return count.most_common(1)[0][0]
+
+        # hashmap
