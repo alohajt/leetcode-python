c3223a68f29c1b10cc3f384d31953d301eefbd91
alohajt <alohajiatao@gmail.com>
2020-10-03 21:03:19 -0600

    429

--- 2. binary tree/429. N-ary Tree Level Order Traversal.py
+++ 2. binary tree/429. N-ary Tree Level Order Traversal.py
@@ -1,9 +1,53 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
+"""
+
+
 class Solution:
     def levelOrder(self, root: 'Node') -> List[List[int]]:
-        if not root: return []
-        queue = [root]
-        res = []
+        """
+        :type root: Node
+        :rtype: List[List[int]]
+        """
+        if not root:
+            return []
+
+        queue = [(root, 0)]
+        res = [[]]
+
         while queue:
-            res.append(node.val for node in queue)
-            queue = [child for node in queue for child in node.children]
+            node, level = queue.pop(0)
+            if level >= len(res):
+                res.append([])
+            res[level].append(node.val)
+            for child in node.children:
+                queue.append((child, level + 1))
         return res
+
+# BFS 模板
+# class Solution(object):
+#     def levelOrder(self, root):
+#         """
+#         :type root: Node
+#         :rtype: List[List[int]]
+#         """
+#         res = []
+#         que = collections.deque()
+#         que.append(root)
+#         while que:
+#             level = []
+#             size = len(que)
+#             for _ in range(size):
+#                 node = que.popleft()
+#                 if not node:
+#                     continue
+#                 level.append(node.val)
+#                 for child in node.children:
+#                     que.append(child)
+#             if level:
+#                 res.append(level)
+#         return res
